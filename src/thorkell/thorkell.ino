fca402358a8f20778b5db1fb1236c193c713c4e6
gustavoriverag <gustavo.rivera@ug.uchile.cl>
2023-07-19 00:12:40 -0400

    deleted duplicate ino file

--- src/thorkell/thorkell.ino
+++ src/thorkell/thorkell.ino
@@ -11,7 +11,7 @@ int pin_mot_1_r = 6;
 int pin_mot_2 = 10;
 int pin_mot_2_r = 11;
 
-int pin_tracker = 7;
+int pin_tracker = 12;
 int pin_trig = 3;
 int pin_echo = 4;
 int pin_button = 2;
@@ -22,7 +22,8 @@ bool batalla = false;
 
 //Variable que indica si el robot busca o avanza
 bool busca = true;
-bool bstate = false;
+//Variable que indica detección de línea
+bool linea = false;
 
 Button  boton(pin_button);
 //Variable que guarda la distancia del sensor ultrasónico)
@@ -43,7 +44,7 @@ void setup()
   
   Serial.begin(9600);
   delay(2000);
-  Serial.println("Ready");
+//  Serial.println("Ready");
 }
 
 int getDist() {
@@ -60,40 +61,77 @@ int getDist() {
   return int(pulso * 0.034 / 2);
 }
 
+bool detectLine(bool lastVal) {
+  //Sólo revisar si es que no se está actualmente sobre la línea
+  if (!lastVal) {
+    bool newVal = digitalRead(pin_tracker);  
+    if (newVal) {
+      return true;
+    }
+  }
+  
+}
+void turn_left(double pot) {
+  analogWrite(pin_mot_1, pot * (double) 255);
+  analogWrite(pin_mot_1_r, 0);
+  analogWrite(pin_mot_2, 0);
+  analogWrite(pin_mot_2_r, pot * (double) 255);
+}
+
+void turn_right(double pot) {
+  analogWrite(pin_mot_1_r, pot * (double) 255);
+  analogWrite(pin_mot_1, 0);
+  analogWrite(pin_mot_2_r, 0);
+  analogWrite(pin_mot_2, pot * (double) 255);
+}
+
+void forward(double pot) {
+  analogWrite(pin_mot_1, pot * (double) 255);
+  analogWrite(pin_mot_1_r, 0);
+  analogWrite(pin_mot_2_r, 0);
+  analogWrite(pin_mot_2, pot * (double) 255);
+}
+
+void backwards(double pot) {
+  analogWrite(pin_mot_1_r, pot * (double) 255);
+  analogWrite(pin_mot_1, 0);
+  analogWrite(pin_mot_2, 0);
+  analogWrite(pin_mot_2_r, pot * (double) 255);
+}
+
+void stop() {
+  analogWrite(pin_mot_1, 0);
+  analogWrite(pin_mot_2, 0);
+  analogWrite(pin_mot_1_r, 0);
+  analogWrite(pin_mot_2_r, 0);
+}
+
 void batallar() {
   if (batalla) {
     dist = getDist();
     //Hay 2 estados, búsqueda y ataque
     if (busca) {
-      analogWrite(pin_mot_1, 180);
-      analogWrite(pin_mot_1_r, 0);
-      analogWrite(pin_mot_2, 0);
-      analogWrite(pin_mot_2_r, 180);
-    
-      if (dist < 30) {
-        Serial.println("Detectado");
+      turn_left((double)180/255);
+      if (dist < 35) {
+//        Serial.println("Detectado");
         busca = false;
       }
     } else {
-      analogWrite(pin_mot_1, 255);
-      analogWrite(pin_mot_1_r, 0);
-      analogWrite(pin_mot_2, 255);
-      analogWrite(pin_mot_2_r, 0);
-      if (dist >=30) {
+      forward(1);
+      if (dist >=35) {
         busca = true; 
       }
     }
     
   } else {
-    analogWrite(pin_mot_1, 0); 
-    analogWrite(pin_mot_1_r, 0);
-    analogWrite(pin_mot_2, 0);
-    analogWrite(pin_mot_2_r, 0);
+    stop();
   }
 }
 void loop()
 {
-
+  // true si el tracker lee 0 (blanco)
+  linea = !digitalRead(pin_tracker);
+  
   // Si hay lectura en el botón, se inicia o se acaba la batalla, y se dejan pasar 5 segundos
   if (boton.toggled() && boton.read() != Button::PRESSED) {
     Serial.println(batalla);
@@ -104,11 +142,28 @@ void loop()
     batalla = !batalla;
     delay(5000);   
   }
-//   if (boton.toggled()) {
-//        if (boton.read() == Button::PRESSED)
-//            Serial.println("Button 3 has been pressed");
-//        else
-//            Serial.println("Button 3 has been released");
-//    }
-//  Serial.println(digitalRead(pin_tracker));
+  if (batalla) {
+    if (linea) {
+//      Serial.println("Se cae");
+      backwards(1);
+    } else {
+      dist = getDist();
+//      Serial.println(dist);
+      //Hay 2 estados, búsqueda y ataque
+      if (busca) {
+        turn_left((double)180/255);
+        if (dist < 30) {
+//          Serial.println("Detectado");
+          busca = false;
+        }
+      } else {
+        forward(1);
+        if (dist >=30) {
+          busca = true; 
+        }
+      }
+    }
+  } else {
+    stop();
+  }
 }
